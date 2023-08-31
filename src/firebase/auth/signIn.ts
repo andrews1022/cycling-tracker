a6529f461eabe62531258c00a4ca87b24f5ac321
Andrew Shearer <andrewshearerdev@gmail.com>
2023-08-31 13:57:26 -0700

    created environment variables and starting setting up firebase auth

---
+++ src/firebase/auth/signIn.ts
@@ -0,0 +1,30 @@
+import { getRedirectResult, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { auth } from "../config";
+
+const provider = new GoogleAuthProvider();
+
+signInWithRedirect(auth, provider);
+
+getRedirectResult(auth)
+  .then((result) => {
+    if (result) {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
