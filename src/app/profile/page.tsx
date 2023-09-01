1aed56f33c8d1b1da696457a19feb3619f05fab3
Andrew Shearer <andrewshearerdev@gmail.com>
2023-08-31 17:10:11 -0700

    created CreateExercise component and added it to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,29 @@
+import CreateExercise from "@/components/CreateExercise";
 import PageHeader from "@/components/PageHeader";
 
 const ProfilePage = () => {
   return (
     <div>
       <PageHeader pageName="ProfilePage" />
+
+      <CreateExercise />
+
+      <p>data goes here</p>
     </div>
   );
 };
 
 export default ProfilePage;
+
+/*
+  FORM (Exercise)
+
+  - fields for:
+    - route name --> string
+    - distance travelled --> number
+    - duration (minutes) --> number
+    - average speed (kmph) --> number
+    - top speed (kmph) --> number
+    - calories burned --> number
+    - date (today's day, month, year) --> date
+*/
---
+++ src/components/CreateExercise.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { useState } from "react";
+import type { FormEvent } from "react";
+
+const CreateExercise = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("form submitted!");
+  };
+
+  return (
+    <form className="mt-4" onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label htmlFor="route_name">
+          <p>Route name</p>
+          <input
+            type="text"
+            name="route_name"
+            id="route_name"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="distance_travelled">
+          <p>Distance travelled (km)</p>
+          <input
+            type="number"
+            name="distance_travelled"
+            id="distance_travelled"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="duration">
+          <p>Duration (minutes)</p>
+          <input
+            type="text"
+            name="duration"
+            id="duration"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="average_speed">
+          <p>Average speed (kmph)</p>
+          <input
+            type="number"
+            name="average_speed"
+            id="average_speed"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="top_speed">
+          <p>Top speed (kmph)</p>
+          <input
+            type="number"
+            name="top_speed"
+            id="top_speed"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="calories_burned">
+          <p>Calories burned</p>
+          <input
+            type="number"
+            name="calories_burned"
+            id="calories_burned"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="date">
+          <p>Date</p>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            className="border-2 border-solid border-cyan-600"
+          />
+        </label>
+      </div>
+
+      <div>
+        <button
+          className="border-cyan-600 border-solid border-2 py-2 px-6 hover:bg-cyan-600 hover:text-white"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CreateExercise;
